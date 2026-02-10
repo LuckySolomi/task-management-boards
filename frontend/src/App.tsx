f3d89d630d3ec3c821a83c5e77daec0169cfa999
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2026-02-10 13:49:12 +0200

    Configures store.ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,32 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setBoard } from "./features/board/boardSlice";
+import { getBoard } from "./api/boardAPI";
+import type { RootState } from "./app/store";
+
+const BOARD_ID = "ueb0o3"; // встав свій boardId з бекенду
+
 function App() {
+  const dispatch = useDispatch();
+  const board = useSelector((state: RootState) => state.board);
+
+  useEffect(() => {
+    const loadBoard = async () => {
+      try {
+        const data = await getBoard(BOARD_ID);
+        dispatch(setBoard({ boardId: data.boardId, name: data.name }));
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    loadBoard();
+  }, [dispatch]);
+
   return (
-    <>
-      <p>Hello from App.tsx!</p>
-    </>
+    <div>
+      <h1>{board.name || "Loading..."}</h1>
+      {/* Тут потім будемо рендерити Board, Column, Card */}
+    </div>
   );
 }
 
